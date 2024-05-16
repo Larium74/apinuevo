3500fe286bbd30e4c082338de5603befa463b6f3
yeizer74 <yeimer.z@outlook.com>
2024-05-16 13:03:45 -0500

    agregando base de las rutas

--- routes/clientes.routes.js
+++ routes/clientes.routes.js
@@ -4,7 +4,6 @@ import { getClientes, mainRoute, postClientes } from "../controllers/clientes.js
 let routerClientes = Router ()
 
 
-routerClientes.get ("/", mainRoute)
 routerClientes.get ("/clientes", getClientes)
 routerClientes.post ("clientes", postClientes)
 
