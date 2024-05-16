bd7b7ef909cd5d8c6c5889c943596595907e5bb4
yeizer74 <yeimer.z@outlook.com>
2024-05-16 12:32:00 -0500

    agregando middlewheres

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express"
 import connectiondb from "./database/connection.js"
 import routerClientes from "./routes/clientes.routes.js"
+import cors from "cors"
+
 
 let app = express ()
 const PORT = 3000
+
+app.use (cors ())
+app.use (express.json ())
+app.use (express.urlencoded ({extended: false}))
+
 app.use (routerClientes)
 
 
