5401bd059c8698526a3e8cdc7651850f5a167a12
yeizer74 <yeimer.z@outlook.com>
2024-05-16 13:21:22 -0500

    agregando states

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -2,7 +2,10 @@ import connectiondb from "../database/connection.js";
 
 export let getClientes = (req, res) => {
     console.log ("Accediendo a la ruta get /clientes")
-    res.send ("Aquí se mostrarán los clientes")
+    res.status (200).json ({
+        message: "Ruta de los Clientes",
+        state: "successly"
+    })
 
 }
 
@@ -12,5 +15,8 @@ export let postClientes = (req, res) => {
 
 export let mainRoute = (req, res) => {
     console.log ("Accdiendo a la ruta principal /")
-    res.send ("Bienvenidos a la ruta principal")
+    res.status (299).json ({
+        message: "Bienvenidso a la ruta principal /",
+        state: "successly"
+    })
 }
